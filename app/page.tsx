8ebf278fd20fc4dbaa3c2a7995d94399658dd011
marciodavid <marciodavid81@gmail.com>
2024-04-30 19:41:48 -0300

    feat: add category itens and list components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
+import CategoryList from "@/components/category-list";
 import Header from "@/components/header";
 import Search from "@/components/search";
 
 const Home = () => {
-  return  <> 
+  return  (
+  <> 
   <Header />
   <div className="px-5 pt-6">
   <Search />
   </div>
-  </>;
+  <div className="px-5 pt-6">
+  <CategoryList />
+  </div>
+  </>
+  );
 };
 
 export default Home;
--- components/category-list.tsx
+++ components/category-list.tsx
@@ -1,5 +1,15 @@
-const CategoryList = () => {
-    return (  );
-}
+import { db } from "@/lib/prisma";
+import CategoryItem from "./category-item";
+
+const CategoryList = async () => {
+    const categories = await db.category.findMany({});
+    return (
+    <div className=" grid grid-cols-3 gap-3">
+    {categories.map((category) => (
+    <CategoryItem key={category.id} category={category} />
+    ))}
+    </div>
+    );
+};
  
 export default CategoryList;
