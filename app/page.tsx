dc7bd1ad22d0e9c6af61403051ba5fa14374c813
marciodavid <marciodavid81@gmail.com>
2024-04-30 21:29:17 -0300

    feat: add promobanner to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "@/components/category-list";
 import Header from "@/components/header";
 import Search from "@/components/search";
@@ -6,12 +7,27 @@ const Home = () => {
   return  (
   <> 
   <Header />
+
   <div className="px-5 pt-6">
   <Search />
   </div>
+
   <div className="px-5 pt-6">
   <CategoryList />
   </div>
+
+  <div className="px-5 pt-6">
+    <Image
+    src="/promobanner01.png"
+    alt="Até 30% de desconto em Pizzas Selecionadas"
+    height={0}
+    width={0}
+    className="w-full h-auto object-contain"
+    sizes="100vw"
+    quality={100}
+    />
+  </div>
+    
   </>
   );
 };
