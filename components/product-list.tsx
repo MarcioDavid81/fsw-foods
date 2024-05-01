d60672809fa4c7e364e937b8ec9d0a27cfb8fc78
marciodavid <marciodavid81@gmail.com>
2024-04-30 22:48:58 -0300

    feat: add listen products

---
+++ components/product-list.tsx
@@ -0,0 +1,22 @@
+import { db } from "@/lib/prisma";
+import ProductItem from "./product-item";
+
+const ProductList = async () => {
+    const products = await db.product.findMany({
+        where: {
+            discountPercentage: {
+                gt: 0,
+            },
+        },
+    });
+   
+    return ( 
+        <div>
+            {products.map((product) => (
+                <ProductItem key={product.id} product={product} />
+            ))}
+        </div>
+     );
+};
+ 
+export default ProductList;
